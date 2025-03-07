8c56fb481309b7fb0915667e440c567b332dcced
Arun prasath N <aruntendulkar2000@gmail.com>
2025-03-08 00:53:33 +0530

    Empty application

---
+++ src/main/java/com/example/radixsorting/RadixsortingApplication.java
@@ -0,0 +1,13 @@
+package com.example.radixsorting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadixsortingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadixsortingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/radixsorting/RadixsortingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.radixsorting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadixsortingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
